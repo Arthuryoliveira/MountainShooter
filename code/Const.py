365ab8fdd4bdda1d66e36d3c3b87e82b0dd8547c
ArthurYan <arthuryan2010@hotmail.com>
2026-02-18 21:41:47 -0300

    Creating meu interations Creating ABC class Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -2,10 +2,22 @@
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 
+#E
+ENTITY_SPEED ={
+    'level1Bg0': 0,  # primeira não mexe
+    'level1Bg1': 1,
+    'level1Bg2': 2,
+    'level1Bg3': 3,
+    'level1Bg4': 4,
+    'level1Bg5': 5,
+    'level1Bg6': 6,
+}
+
+
 #M
 MENU_OPTION =('NEW GAME 1P',
               'NEW GAME 2P - COOPERATIVE',
-              'NEW GAME 3P - COOPERATIVE',
+              'NEW GAME 2P - COMPETITIVE',
               'SCORE',
               'EXIT')
 
@@ -13,3 +25,5 @@ MENU_OPTION =('NEW GAME 1P',
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
 
+# Y
+COLOR_YELLOW = (255, 255, 0)
--- code/level.py
+++ code/level.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Level:
-    def __init__(self):
-        self.window = None
-        self.name = None
-        self.entity_list = None
-        self.entity_list = None
+import pygame
+from code.entity import Entity
+from code.entityFactory import EntityFactory
 
-    def run(self, ):
-        pass
+
+class Level:
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('level1Bg'))
+
+    def run(self):
+        clock = pygame.time.Clock()
+
+        while True:
+            clock.tick(60)  # limita a 60 FPS
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            for ent in self.entity_list:
+                self.window.blit(ent.surf, ent.rect)
+                ent.move()
+
+            pygame.display.flip()
+
+
+def level():
+    return None
