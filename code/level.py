926f0b1156a2abf81b5c7e8db9c5f652336293bc
ArthurYan <arthuryan2010@hotmail.com>
2026-02-19 22:53:52 -0300

    Fixing imports in level.py file

--- code/level.py
+++ code/level.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
 
 import pygame
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import COLOR_WHITE, WIN_HEIGHT
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
@@ -13,8 +19,11 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('level1Bg'))
+        self.timeout = 40000  # 40 Segundos
 
     def run(self):
+        pygame.mixer.music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer.music.play(-1)
         clock = pygame.time.Clock()
 
         while True:
@@ -23,14 +32,20 @@ class Level:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    quit()
+                    sys.exit()
 
             for ent in self.entity_list:
                 self.window.blit(ent.surf, ent.rect)
                 ent.move()
 
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {int(clock.get_fps())}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+
             pygame.display.flip()
 
-
-def level():
-    return None
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
